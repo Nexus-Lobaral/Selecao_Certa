7af21112406fe6cf6ab546b210af8b8d5a923535
Ksanagii <135341036+Ksanagii@users.noreply.github.com>
2024-04-13 20:22:32 -0300

    Aprimoramento da Base
    exclui o a classe cargos pq acho que não precisar mais, e aprimorei e adicionei algumas coisas

--- Desafios RH/Assets/Scripts/Empresas.cs
+++ Desafios RH/Assets/Scripts/Empresas.cs
@@ -1,54 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public abstract class Empresas : MonoBehaviour
 {
     //Atributos
-    
-    //Metodos
+    protected List<string> listaCargos = new List<string>(3);
+    protected string cargo;
 
-    //ainda to pensando em oq colocar aqui kkkkk
+    //Metodos
+    public abstract string DefinirCargo(); // todas empresas terão um metodo de definir cargo
+    //ainda to pensando em oq mais colocar aqui kkkkk
 
 }
-
+/*
 public class Cargos
 {
     // Atributos
-    protected string cargo;
+     // inicializando a variavel cargo
 
     // Construtor
-    public Cargos() {}
 
     // Metodos
-    public void DefinirCargo(List<string> listaCargos) // Ao chamar este metodo, voce coloca a lista de cargos da empresa e então ele escolhera um dos 3 cargos
+    
+    public void DefinirCargo() // 
     {
-        cargo = listaCargos[Random.Range(0, listaCargos.Count)]; 
+        
     }
-    public string Cargo() // Metodo que retorna o nome cargo
+    
+    public string Cargo(List<string> listaCargos) // Ao chamar este metodo, voce coloca a lista de cargos da empresa e então ele escolhera um dos 3 cargos e retorna o nome cargo
     {
-        if (cargo != null)
-        {
-            return cargo;
-        }
-        else
-        {
-            return "Cargo não definido";
-        }
+        cargo = listaCargos[Random.Range(0, listaCargos.Count)];
+        return cargo;
     }
+    
 }
+*/
 
 public class Curriculos
 {
     // Atributos
-    Cargos cargo = new Cargos();
-    GuacamolJuegos GuacamolJuegos = new GuacamolJuegos();
-    protected List<string> nomePessoa = new List<string>() {};// Gerar nome de pessoa aleatoriamente pelo ChatGpt (se for dificil demais, deixa para depois do prototipo)
+    Empresas empresa;
+    protected string cargo;
+    protected List<string> listaPessoas = new List<string>() {"João", "Maria", "Enzo", "Marcelo"}; // Gerar nome de pessoa aleatoriamente pelo ChatGpt (se for dificil demais, deixa para depois do prototipo)
+    protected string nomePessoa;
+    protected string caracteristicas;
+    protected string detalhes;
 
     // Construtor
-    
-
+    public Curriculos(Empresas empresa) // construo um curriculo, precisando de uma empresa de referencia para pegar os dados
+    {
+        this.empresa = empresa;
+        cargo = empresa.DefinirCargo();
+        nomePessoa = listaPessoas[Random.Range(0, listaPessoas.Count)]; // Definindo o nome da pessoa do curriculo
+    }
 
     // Metodos
-    
+
 }
--- Desafios RH/Assets/Scripts/GuacamolJuegos.cs
+++ Desafios RH/Assets/Scripts/GuacamolJuegos.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuacamolJuegos : Empresas
+public class GuacamolJuegos : Empresas // Empresa de jogos que herda da classe de empresa
 {
     // Atributos
     protected string descricao; // Descrição da empresa que aparecerá no card quando for selecionar a empresa
-    private List<string> listaCargosDJogos = new List<string>() { "GameDev", "Produtor Executivo", "Designer" }; // Lista de cargos
+    
     // Construtores
-    public GuacamolJuegos() { }
+    public GuacamolJuegos() 
+    {
+        listaCargos = new List<string> { "GameDev", "Produtor Executivo", "Designer" }; // Lista de cargos especifico desta empresa
+    }
 
     // Metodos
     public string MostrarDescricao() // Metodo para retornar a variavel de descrição
     {
         return descricao;
+    }
+    public List<string> MostrarListaCargos() // // Metodo para retornar Lista de cargos
+    {
+        return listaCargos;
+    }
+    public override string DefinirCargo() // Ao chamar este metodo, ele escolhera um dos 3 cargos e retorna o nome cargo
+    {
+        cargo = listaCargos[Random.Range(0, listaCargos.Count)];
+        return cargo;
+
     }
 
 }
