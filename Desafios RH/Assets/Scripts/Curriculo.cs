3c3066e5457d25ef40612e5ee010a92c31338856
zazzymann <jvrpcanal@gmail.com>
2024-04-26 17:15:40 -0300

    002

--- Desafios RH/Assets/Scripts/Curriculo.cs
+++ Desafios RH/Assets/Scripts/Curriculo.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class Curriculos : MonoBehaviour 
+public class Curriculo : MonoBehaviour
 {
     // Atributos
     Empresas empresa;
     protected string cargo;
-    protected List<string> listaPessoas = new List<string>() { "João", "Maria", "Enzo", "Marcelo" }; // Gerar nome de pessoa aleatoriamente pelo ChatGpt (se for dificil demais, deixa para depois do prototipo)
+    protected List<string> listaPessoas = new List<string>() { "Joï¿½o", "Maria", "Enzo", "Marcelo" }; // Gerar nome de pessoa aleatoriamente pelo ChatGpt (se for dificil demais, deixa para depois do prototipo)
     protected string nomePessoa;
     protected string caracteristicas;
     protected string detalhes;
 
     // Construtor
-    public Curriculos(Empresas empresa) // construo um curriculo, precisando de uma empresa de referencia para pegar os dados
+    public Curriculo(Empresas empresa) // construo um curriculo, precisando de uma empresa de referencia para pegar os dados
     {
         this.empresa = empresa;
         cargo = empresa.DefinirCargo();
@@ -22,5 +21,7 @@ public class Curriculos : MonoBehaviour
     }
 
     // Metodos
+
+
 
 }
